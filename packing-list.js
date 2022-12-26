c2745c62977e6104f8cf26a1a22858dbf10c454c
Lupita Reyes <lupita.g.reyes@outlook.com>
2022-12-26 15:43:33 -0500

    using for loops in arrays

---
+++ packing-list.js
@@ -0,0 +1,10 @@
+//declare an arrar
+const packingList = ["bowls", "plates", "pots", "pans", "eating utensils", "glasses", "cups", "cooking utensils"];
+
+//use for loop
+console.log("Kitchen stuff to pack");
+for (let i = 0; i < packingList.length; i++) {
+  console.log(packingList[i]);
+}
+
+packingList[3]
